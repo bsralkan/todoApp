6c7720b66a7b44b268edef9f2dbd6db92ba12c20
bsralkan <bsr.alkan@gmail.com>
2020-10-01 14:37:10 +0300

    3-Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Büşra';
+  items=[
+    {description:"Kahvaltı", action:"No"},
+    {description:"Sinema", action:"No"},
+    {description:"Spor", action:"No"},
+    {description:"Ders Çalışma", action:"No"}
+  ]
 }
